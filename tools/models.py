5bedb96c6a7a9c97c0d64a262885f5efe4aaf280
andreasHovaldt <andreas.hovaldt@gmail.com>
2025-09-18 10:05:17 +0200

    Moved models and utils to their own files

---
+++ tools/models.py
@@ -0,0 +1,58 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class SimpleMultiTaskModel(nn.Module):
+    def __init__(self, input_dim, actuator_output_dim, mode_output_dim):
+        super(SimpleMultiTaskModel, self).__init__()
+        
+        self.layer1 = nn.Linear(in_features=input_dim, out_features=128)
+        self.layer2 = nn.Linear(in_features=128, out_features=128)
+        self.regression_head = nn.Linear(in_features=128, out_features=actuator_output_dim)
+        self.classification_head = nn.Linear(in_features=128, out_features=mode_output_dim)
+        
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        x_actuators = F.relu(self.regression_head(x))
+        x_mode = F.softmax(self.classification_head(x), dim=1)
+        
+        return x_actuators, x_mode
+    
+
+
+class SimpleClassificationModel(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SimpleClassificationModel, self).__init__()
+        
+        self.layer1 = nn.Linear(in_features=input_dim, out_features=128)
+        self.layer2 = nn.Linear(in_features=128, out_features=128)
+        self.classification_head = nn.Linear(in_features=128, out_features=output_dim)
+        
+        self.relu = nn.LeakyReLU()
+        self.softmax = nn.Softmax(dim=1)
+        
+    def forward(self, x):
+        x = self.relu(self.layer1(x))
+        x = self.relu(self.layer2(x))
+        return self.softmax(self.classification_head(x))
+
+
+
+class HVACModeMLP(nn.Module):
+    def __init__(self, in_dim: int, n_classes: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(in_dim, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, n_classes)
+        )
+    def forward(self, x):
+        return self.net(x)
